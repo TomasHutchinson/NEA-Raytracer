edd38eeee64d596ce56131388e13cc8817ff37f0
Tomás Hutchinson <tpshutch@hotmail.com>
2025-04-01 00:36:41 +0100

    x / y chunking

--- renderer.py
+++ renderer.py
@@ -2,17 +2,15 @@ from PIL import Image
 import time
 import numpy as np
 
-def process_chunk(x_start, x_end, resolution):
-    chunk = np.zeros((resolution[1], x_end - x_start, 3), dtype=np.uint8)
+def process_chunk(x_start, x_end, y_start, y_end, resolution):
+    chunk = np.zeros((y_end - y_start, x_end - x_start, 3), dtype=np.uint8)
     for x in range(x_start, x_end):
-        for y in range(resolution[1]):
+        for y in range(y_start, y_end):
             color = pixel(x / resolution[0], y / resolution[1])
-            blah = 0#sum(range(1000)) #Simulating the extra computation  PLEASE REMOVE ME
-            for i in range(1000): blah += 1
-            chunk[y, x - x_start] = (int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
-    return x_start, chunk
+            chunk[y - y_start, x - x_start] = (int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
+    return x_start, y_start, chunk
 
-def render(resolution: tuple, func, num_x_chunks=4, num_y_chunks=4) -> Image:
+def render(resolution: tuple, num_x_chunks=4, num_y_chunks=4) -> Image:
     starttime = time.process_time()
     
     x_chunk_size = resolution[0] // num_x_chunks
@@ -27,7 +25,7 @@ def render(resolution: tuple, func, num_x_chunks=4, num_y_chunks=4) -> Image:
             y_start = j * y_chunk_size
             y_end = (j + 1) * y_chunk_size if j < num_y_chunks - 1 else resolution[1]
             
-            x_start, y_start, chunk = process_chunk(x_start, x_end, y_start, y_end, resolution, func)
+            x_start, y_start, chunk = process_chunk(x_start, x_end, y_start, y_end, resolution)
             image_array[y_start:y_start + chunk.shape[0], x_start:x_start + chunk.shape[1]] = chunk
     
     frame = Image.fromarray(image_array, mode='RGB')
@@ -36,4 +34,7 @@ def render(resolution: tuple, func, num_x_chunks=4, num_y_chunks=4) -> Image:
 
 
 def pixel(u,v):
+    blah = 0
+    for i in range(1000):
+        blah += 1
     return (u,v,1)
