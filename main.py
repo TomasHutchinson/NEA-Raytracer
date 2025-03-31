57a534d16a6d85d37d4f15f3bc8b80e21edc10d3
Tomás Hutchinson <tpshutch@hotmail.com>
2025-03-31 19:58:05 +0100

    Basic UI created

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import customtkinter as ctk
+
+#custom files
+import user_interface
+
 print("Hello World")
 
 ctk.set_appearance_mode("dark")# Modes: system (default), light, dark
 ctk.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
-root = ctk.CTk()
 
-def button_callback():
-    print("button pressed")
+root = user_interface.Root()
 
-button = ctk.CTkButton(root, text="my button", command=button_callback)
-button.grid(row=0, column=0, padx=20, pady=20)
-
-while 1:
-    root.update()
+root.mainloop()
---
+++ user_interface.py
@@ -0,0 +1,115 @@
+import customtkinter as ctk
+from PIL import Image
+
+class SceneTreeFrame(ctk.CTkScrollableFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master, label_text=title)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.checkboxes = []
+
+        for i, value in enumerate(self.values):
+            checkbox = ctk.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
+
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
+
+class BottomTabs(ctk.CTkTabview):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        #add the necessary tabs
+        self.add("Render Details")
+        self.add("Console")
+
+        ### Render Details ###
+        self.label = ctk.CTkLabel(master=self.tab("Render Details"), text= "Imagine render details here")
+        self.label.grid(row=0, column=0, padx=20, pady=10)
+
+        ### Console ###
+        self.label = ctk.CTkLabel(master=self.tab("Console"), text="Imagine console-y stuff here")
+        self.label.grid(row=0, column=0, padx=20, pady=10)
+
+class FileBox(ctk.CTkScrollableFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master, label_text=title)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.checkboxes = []
+
+        for i, value in enumerate(self.values):
+            checkbox = ctk.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
+
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
+
+class RenderPreview(ctk.CTkFrame):
+    image = ctk.CTkImage
+    image_label = ctk.CTkLabel
+    def __init__(self, master, img, **kwargs):
+        super().__init__(master, **kwargs)
+        self.image = img
+        self.image_label = ctk.CTkLabel(self, image = self.image, text="")
+
+        self.image_label.grid()
+
+class PropertyEditor(ctk.CTkScrollableFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master, label_text=title)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.checkboxes = []
+
+        for i, value in enumerate(self.values):
+            checkbox = ctk.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
+
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
+
+class Root(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Get Raytraced")
+        self.geometry("1280x720")
+        self.grid_columnconfigure(0, weight=0)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure((0, 1), weight=1)
+
+        values = ["value 1", "value 2", "value 3", "value 4", "value 5", "value 6"]
+        self.scenetree_frame = SceneTreeFrame(self, title="Scene Tree", values=values)
+        self.scenetree_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+
+        self.property_editor = PropertyEditor(self, title="Property Editor", values=values)
+        self.property_editor.grid(row=0,column=2, rowspan=2, padx=10, pady=10, sticky="nwse")
+       
+        self.bottom_tabs = BottomTabs(master=self)
+        self.bottom_tabs.grid(row=1, column=1, padx=1, pady=10, sticky="nwse")
+
+        self.filebox = FileBox(self, title="FileSystem", values=values)
+        self.filebox.grid(row=1, column=0, padx=1, pady=(10, 0), sticky="nsw")
+
+        tmpimage = ctk.CTkImage(dark_image=Image.open("C:\\Users\\TPSHu\\Downloads\\IMG_4142.png"), size=(640,480))
+        self.render_preview = RenderPreview(self, tmpimage)
+        self.render_preview.grid(row=0, column=1, padx=10, pady=10, sticky="nsw")
+
+        
+
